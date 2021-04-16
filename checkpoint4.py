12bea23f65d800864bdbc45a93173aaf6d0f98e4
Trevor Bisson <bissot@rpi.edu>
2021-04-15 21:55:02 -0400

    cp 4

--- checkpoint4.py
+++ checkpoint4.py
@@ -1,5 +1,19 @@
 from pymongo import MongoClient
+from pprint import pprint
 client = MongoClient()
 
 if __name__ == '__main__':
     print("Modify me")
+    # establishing connection 
+    # to the database
+    client = MongoClient("mongodb://localhost:27017/") 
+    
+    # Database name 
+    db = client["mongo_db_lab"] 
+    
+    # Collection name 
+    col = db["definitions"] 
+  
+    # if we don't want to print id then pass _id:0
+    for document in col.find({}, {}): 
+        pprint(document)
